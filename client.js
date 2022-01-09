ea6585e860d67d983ab8efeef54699b52ee51236
AmmarKareem <75568336+AmmarKareem@users.noreply.github.com>
2022-01-09 11:26:53 -0500

    Add files via upload

---
+++ client.js
@@ -0,0 +1,18 @@
+function scrollToAboutMeDiv() {
+    let elem = document.getElementById("aboutMe");
+    elem.scrollIntoView();
+    return false;
+}
+
+function scrollToProjectsDiv() {
+    let elem = document.getElementById("projects");
+    elem.scrollIntoView();
+    return false;
+}
+
+function scrollToResumeDiv() {
+    let elem = document.getElementById("resume");
+    elem.scrollIntoView();
+    return false;
+}
+
